af058679e6e98ab2f27a9443e5dd83a1bedd61ce
Oscar Romero <ro23001@esfe.agape.edu.sv>
2023-09-13 16:46:05 -0600

    se arreglaron factura y detalle de factura finish

--- SysInventarioFacturacion.EntidadesDeNegocio/Factura.cs
+++ SysInventarioFacturacion.EntidadesDeNegocio/Factura.cs
@@ -37,14 +37,22 @@ namespace SysInventarioFacturacion.EntidadesDeNegocio
         [Required(ErrorMessage = "Correo es obligatorio")]
         [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
         public string? Correo { get; set; }
-        [Required(ErrorMessage = "Total es obligatorio")]
-        public decimal Total { get; set; }
-        [Required(ErrorMessage = "Descuento es obligatorio")]
-        public decimal Descuento { get; set; }
-        [Required(ErrorMessage = "Impuesto es obligatorio")]
-        public decimal Impuesto { get; set; }
-        [Required(ErrorMessage = "Total pagado es obligatorio")]
-        public decimal TotalPagado { get; set; }
+        [Display(Name = "Total")]
+        [Required(ErrorMessage = "El campo Total es obligatorio.")]
+        public decimal? Total { get; set; }
+        [Display(Name = "Descuento")]
+        [Required(ErrorMessage = "El campo Descuento es obligatorio.")]
+        [Range(0.01, 10000, ErrorMessage = "El campo Precio debe estar entre 0.01 y 10,000.")]
+        public decimal? Descuento { get; set; }
+        [Display(Name = "Impuesto")]
+        [Required(ErrorMessage = "El campo Impuesto es obligatorio.")]
+        [Range(0.01, 10000, ErrorMessage = "El campo Precio debe estar entre 0.01 y 10,000.")]
+        public decimal? Impuesto { get; set; }
+
+        [Display(Name = "Total Pagado")]
+        [Required(ErrorMessage = "El campo Total Pagado es obligatorio.")]
+        [Range(0.01, 10000, ErrorMessage = "El campo Precio debe estar entre 0.01 y 10,000.")]
+        public decimal? TotalPagado { get; set; }
 
         public Usuario? Usuario { get; set; }
        
