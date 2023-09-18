0315a0d8373e85b09c38d540fd1c9a49bae95a99
AlfonsoESFE <cm23009@esfe.agape.edu.sv>
2023-09-18 16:35:11 -0600

    Se arreglo esta mamada

---
+++ SysInventarioFacturacion.UI.AppWebAspNetCore/wwwroot/js/Menu.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    var $menuToggle = $('.menu-toggle');
+    var $dashboardNav = $('.dashboard-nav-list');
+
+    $menuToggle.on('click', function () {
+        $dashboardNav.toggleClass('menu-hidden');
+    });
+});
